5edbee705d76b97e14469ebffc16cdafcf8bf2e1
jiho1996 <jiho.urssu@gmail.com>
2022-08-07 18:22:19 +0900

    fix : 게임이 끝나지 않던 현상 해결

--- js/end-to-end-game/index.js
+++ js/end-to-end-game/index.js
@@ -85,7 +85,7 @@ function App(){
         LIST.incorrect_list[$order.textContent - 1] -= 1;
         console.log(LIST.incorrect_list);
         $warn.textContent = RESULT_TEXT.RESULT_FAIL + `남은기회 ${LIST.incorrect_list[$order.textContent - 1]}`
-        if (LIST.incorrect_list[$order.textContent] < 0){
+        if (LIST.incorrect_list[$order.textContent - 1] < 0){
             alert(`탈락자는 ${$order.textContent}번째 참가자`)
             location.reload();
         }
